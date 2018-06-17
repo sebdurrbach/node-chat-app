1a7fa49c881e4f2a85b7429933c26d44263d9c9a
sdurrbach <sebastien.durrbach@gmail.com>
2018-06-17 23:58:28 +0200

    Add connection/disconnection with socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
+// @ts-check
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 let app = express();
+// Pour utiliser socket.io, on doit créer un server et utiliser http à la place de express
+let server = http.createServer(app);
+// On passe ensuite le serveur http par socket.io dans une nouvelle variable
+// Donne accès à la librairie socket.io côté client à l'adresse /socket.io/socket.io.js
+// On passe l'url en source d'une balise script dans index.html
+let io = socket(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from client');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
