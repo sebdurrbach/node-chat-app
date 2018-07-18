7b461d3013f451fae26add1710fe2124f27c611e
sdurrbach <sebastien.durrbach@gmail.com>
2018-07-18 21:42:39 +0200

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+// Moment est un module super utile pour formater les dates
+// Check docs sur momentjs.com
+const moment = require('moment');
+
+let date = moment();
+
+console.log(date.format('MMM Do Y hh:mm:ss a'));
